8d5f3cc13ea78957d46da7bde6440cff4e9da0f9
vladimirKartyshov <vovan198080@inbox.ru>
2020-03-12 10:21:22 +0300

    Firebase Authentication

--- app/src/main/java/com/example/myandroidchat/SigninActivity.java
+++ app/src/main/java/com/example/myandroidchat/SigninActivity.java
@@ -4,11 +4,17 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SigninActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        auth = FirebaseAuth.getInstance();
     }
 }
