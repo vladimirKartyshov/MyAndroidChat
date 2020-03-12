b813b9bbabf616aa9773072fe2cfad6317402bc6
vladimirKartyshov <vovan198080@inbox.ru>
2020-03-12 12:49:29 +0300

    Регистрация пользователя

--- app/src/main/java/com/example/myandroidchat/SigninActivity.java
+++ app/src/main/java/com/example/myandroidchat/SigninActivity.java
@@ -1,14 +1,33 @@
 package com.example.myandroidchat;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SigninActivity extends AppCompatActivity {
 
+    private static final String TAG = "SigninActivity";
+
     private FirebaseAuth auth;
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private EditText nameEditText;
+    private TextView toggleLoginSignUpTextView;
+    private Button loginSignUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +35,43 @@ public class SigninActivity extends AppCompatActivity {
         setContentView(R.layout.activity_signin);
 
         auth = FirebaseAuth.getInstance();
+
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        nameEditText = findViewById(R.id.nameEditText);
+        toggleLoginSignUpTextView = findViewById(R.id.toggleLoginSignUpTextView);
+        loginSignUpButton = findViewById(R.id.loginSignUpButton);
+
+        loginSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginSignUpUser(emailEditText.getText().toString().trim(),
+                        passwordEditText.getText().toString().trim());//trim,чтобы обрезать пробелы в нач и конце строки
+
+            }
+        });
+    }
+
+    private void loginSignUpUser(String email, String password) {
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user =  auth.getCurrentUser();
+                           // updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SigninActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                           // updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
     }
 }
