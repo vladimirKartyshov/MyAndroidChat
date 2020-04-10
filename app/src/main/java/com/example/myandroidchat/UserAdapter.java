4744005c90abcfce03c408a8cfb752758edaa159
vladimirKartyshov <vovan198080@inbox.ru>
2020-04-10 13:07:59 +0300

    UserAdapter

---
+++ app/src/main/java/com/example/myandroidchat/UserAdapter.java
@@ -0,0 +1,77 @@
+package com.example.myandroidchat;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder> {
+
+    private ArrayList<User> users;
+    private OnUserClickListener listener;
+
+
+    public interface OnUserClickListener{
+        void onUserClick(int position);
+    }
+
+    public void setOnUserClickListener(OnUserClickListener listener){
+        this.listener = listener;
+    }
+
+    public UserAdapter (ArrayList<User> users){
+        this.users = users;
+    }
+
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).
+                inflate(R.layout.user_item, viewGroup, false);
+        UserViewHolder viewHolder = new UserViewHolder(view, listener);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder userViewHolder, int i) {
+        User currentUser = users.get(i);
+        userViewHolder.avatarImageView.setImageResource(currentUser.getAvatarMockUpResource());
+        userViewHolder.userNameTextView.setText(currentUser.getName());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    public static class UserViewHolder extends RecyclerView.ViewHolder {
+
+        public ImageView avatarImageView;
+        public TextView userNameTextView;
+
+        public UserViewHolder(@NonNull View itemView, final OnUserClickListener listener) {
+            super(itemView);
+            avatarImageView = itemView.findViewById(R.id.avatarImageView);
+            userNameTextView = itemView.findViewById(R.id.userNameTextView);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (listener != null){
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION){
+                            listener.onUserClick(position);
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
